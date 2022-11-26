67d299d3a6b3ef1c8db9a7204f058a0495870fa7
Brian Cairns <bcairns@gmail.com>
2022-11-26 11:23:30 -0700

    build to docs folder

---
+++ docs/internal/immutable/chunks/0-eb25673e.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-69a01df7.js";import{default as t}from"../components/pages/_layout.svelte-51044b3d.js";export{t as component,r as shared};
---
+++ docs/internal/immutable/chunks/1-63439518.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-88ea9063.js";export{t as component};
