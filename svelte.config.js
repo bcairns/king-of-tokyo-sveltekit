969af7085fb3dc44ecb7e4c2618db8859d3c720b
Brian Cairns <bcairns@gmail.com>
2022-11-26 11:16:24 -0700

    static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from "svelte-preprocess";
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	},
+    paths: {
+      base: dev ? '' : '/king-of-tokyo-sveltekit',
+    },
+    appDir: 'internal',
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    })
+  },
 	compilerOptions: {
     enableSourcemap: true,
   },
